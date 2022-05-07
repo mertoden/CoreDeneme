65509426681d684ee72a54226fcee33a8f2bc5c3
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-05-07 22:49:05 +0300

    Context sınıfının yapılması, DataAccess ve Migration

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,15 @@ namespace DataAccess.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=")
+            optionsBuilder.UseSqlServer("server=DESKTOP-T5U2SAE;database=CoreBlogDb; integrated security = true;");
+
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
