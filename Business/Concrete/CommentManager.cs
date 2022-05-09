3aedfd5278c788062157c5479a102e137bc28f2f
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-05-09 18:35:30 +0300

    Blog kısmı bitirilildi. Comment yapıldı.

---
+++ Business/Concrete/CommentManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDL _commentdl;
+        public CommentManager(ICommentDL commentdl)
+        {
+            _commentdl = commentdl;
+        }
+        public void CommentAdd(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetList(int id)
+        {
+            return _commentdl.GetListAll(x => x.BlogID == id);
+        }
+    }
+}
