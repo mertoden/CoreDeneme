7faaff9689c5557eb2039a01df56d37952559532
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-05-16 06:44:39 +0300

    About işlemleri yapıldı. ; Contact Sayfası yapıldı.

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    
+    public class AboutManager: IAboutService
+    {
+        IAboutDL _aboutDL;
+        public AboutManager(IAboutDL aboutDL)
+        {
+            _aboutDL = aboutDL;
+        }
+
+        public List<About> GetList()
+        { 
+            return _aboutDL.GetListAll();
+        }
+    }
+}
