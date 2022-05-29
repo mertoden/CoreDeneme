fd4586e65980d446822eb8da5076c8c9c80e2c2f
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-05-29 06:18:08 +0300

    İstatistik kısmı yapıldı ve revize edildi. v1

---
+++ CoreDeneme/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDeneme.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EFBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
+    }
+}
