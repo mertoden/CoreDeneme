d8d62f5de55aa8823c0390b6b0346aaad59e6e61
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-06-05 02:18:28 +0300

    kullanıcının giriş yapması sağlandı ve gerekli değişiklikler yapıdlı. Dashboard a kullanıcı bilgilerini getirmede yenielme yapıdlı.

---
+++ CoreDeneme/Models/UserSignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDeneme.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Please Enter Username")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Please Enter Password")]
+        public string password { get; set; }
+    }
+}
--- CoreDeneme/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDeneme/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
 using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,15 +11,20 @@ using System.Threading.Tasks;
 
 namespace CoreDeneme.ViewComponents.Writer
 {
-   
-    public class WriterAboutOnDashboard: ViewComponent
+
+    public class WriterAboutOnDashboard : ViewComponent
     {
         WriterManager writermanager = new WriterManager(new EFWriterRepository());
 
         Context c = new Context();
+
+
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var username = User.Identity.Name;
+            ViewBag.v = username;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = writermanager.GetWriterByID(writerID);
             return View(values);
