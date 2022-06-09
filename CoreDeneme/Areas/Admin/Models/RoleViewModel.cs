b431fa109052bd28237fd96e0c616ee2d57dc9f7
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-06-09 14:57:31 +0300

    Rol ekleme silme güncelleme işlemleri yapıdlı.

---
+++ CoreDeneme/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDeneme.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Please Write Role Name")]
+        public string name { get; set; }
+    }
+}
